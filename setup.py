be1f271e8bcfff9207f37d2439244394a6ecfe02
Fan Zou <fuz64@psu.edu>
2023-04-12 19:02:44 -0400

    build the first version

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
     name = "fasthr",
-    version = "1.0",
+    version = "1.0.0",
     author = "Fan Zou",
     author_email = "fuz64@psu.edu",
-    description = "A Python function that efficiently and accurately calculates hardness ratios for X-ray sources using a Bayesian approach",
     long_description = readme,
     long_description_content_type = "text/markdown",
     url = "https://github.com/fanzou99/FastHR",
+    packages = find_packages(),
     install_requires = ["numpy", "scipy"],
     classifiers = [
         "Programming Language :: Python :: 3",
